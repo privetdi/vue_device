5e16d6edf5f9cd3914a422767955715bf73943d4
Dmitriy <dmleshi49@gmail.com>
2024-02-14 19:12:01 +0400

    fix(): fix routs for github-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,9 +7,9 @@ import TableVue from '../pages/Table.vue'
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { name: 'device', path: '/device', component: Devices },
-        { name: 'requestsId', path: '/requests/:id', component: Requests },
-        { name: 'table', path: '/table', component: TableVue, alias: '/' },
+        { name: 'device', path: '/vue_device/device', component: Devices },
+        { name: 'requestsId', path: '/vue_device/requests/:id', component: Requests },
+        { name: 'table', path: '/vue_device/table', component: TableVue, alias: '/' },
     ]
 
 })
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://privetdi.github.io/vue_device",
+  base: process.env.NODE_ENV === 'production'
+    ? '/vue_device/'
+    : '/',
   plugins: [vue()],
 })
